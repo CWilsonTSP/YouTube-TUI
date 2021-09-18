63951c40654dc80ce7cc955060abcc361ca1309d
JuRyMoore <jurymoore@gmail.com>
2021-09-18 15:45:41 -0400

    Put data from queries into a list of class objects

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import requests
 import json
 
+class Result:
+    def __init__(self,title,channel,date,ID,thumbnail):
+        self.title = title
+        self.channel = channel
+        self.date = date
+        self.ID = ID
+        self.thumbnail = thumbnail
+
 def main():
     inp = input("Enter a search Query: ")
     query = """https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=""" + inp + """&key=AIzaSyDXxvLRGt4V3p6yzi-34rCz9W0cLcoTVds"""
@@ -8,8 +16,14 @@ def main():
     r = requests.get(query)
     r_dict = r.json()
 
+    results = []
     for item in r_dict["items"]:
-        print(item["snippet"]["title"], item["id"])
+        #print(item["snippet"]["title"], item["id"])
+        if item["id"]["kind"] == "youtube#video":
+            results.append(Result(item["snippet"]["title"],item["snippet"]["channelTitle"],item["snippet"]["publishTime"],item["id"]["videoId"],item["snippet"]["thumbnails"]["high"]["url"]))
+
+    for v in results:
+        print(v.title,v.channel,v.date,v.ID,v.thumbnail)
 
 if __name__ == "__main__":
     main()
