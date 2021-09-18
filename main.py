7ea75ac4571221f39301501f627a6c7c8d715210
Chad Wilson <mrchadmwilson@gmail.com>
2021-09-18 14:55:51 -0400

    transition from c++ to python

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+def main():
+    inp = input("Enter a search Query: ")
+    query = """https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=""" + inp + """&key=AIzaSyDXxvLRGt4V3p6yzi-34rCz9W0cLcoTVds"""
+    # r = requests.get("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=c%2B%2B%20tutorial&key=AIzaSyDXxvLRGt4V3p6yzi-34rCz9W0cLcoTVds")
+    r = requests.get(query)
+    r_dict = r.json()
+
+    for item in r_dict["items"]:
+        print(item["snippet"]["title"], item["id"])
+
+if __name__ == "__main__":
+    main()
