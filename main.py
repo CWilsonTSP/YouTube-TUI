e55098e925aafd4f028f09d4877ff124fa7171cb
Chad Wilson <mrchadmwilson@gmail.com>
2021-09-18 15:01:26 -0400

    encoded query

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import requests
 import json
+import urllib.parse
 
 def main():
     inp = input("Enter a search Query: ")
-    query = """https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=""" + inp + """&key=AIzaSyDXxvLRGt4V3p6yzi-34rCz9W0cLcoTVds"""
+    # print(inp)
+    query = """https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=""" + urllib.parse.quote(inp) + """&key=AIzaSyDXxvLRGt4V3p6yzi-34rCz9W0cLcoTVds"""
     # r = requests.get("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=c%2B%2B%20tutorial&key=AIzaSyDXxvLRGt4V3p6yzi-34rCz9W0cLcoTVds")
     r = requests.get(query)
     r_dict = r.json()
