e9f689dbda0c109e753b067ebf573137a672ea19
Chad Wilson <mrchadmwilson@gmail.com>
2021-09-22 15:29:37 -0400

    seperated meat from main (temporary), and started load_config

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
-import requests
+"""Imports"""
+import os
 import urllib.parse
+import requests
 from result import Result
 
 def load_config():
-    # eventually, these should reference the user's XDG variables
-    # try loading from these locations first
-    # XXX Perhaps, we simply launch the "main" script from a bash script,
-    # as that would allow us to to directly interface with the shell
-    # that we are loading variables from
-    locations = ["$HOME/.config/yttui/yttuirc", "$HOME/.yttuirc"]
-    for loc in locations:
-        ...
-        # if file exists, open it and load it. Then break.
-        # else, create the file with defaults
+    """Load the config file for yttui"""
+    dir_config = ""
+    try:
+        dir_config = os.environ["XDG_CONFIG_HOME"] + "/yttui/"
+    except KeyError:
+        dir_config = os.environ["HOME"] + "/.config/yttui/"
 
-def main():
-    # ask the user for input
+    print("config file is at ", dir_config)
+
+    try:
+        with open(dir_config+"yttui.conf", "r"):
+            print("Loading Config")
+            # XXX load config file
+    except FileNotFoundError:
+        print("Config not found, creating")
+        try:
+            os.mkdir(dir_config)
+        except FileExistsError:
+            pass
+
+        with open(dir_config+"yttui.conf", "w"):
+            # print to this file the config
+            ...
+
+def current():
+    """I just seperated what was in main into its own function for now"""
     inp = input("Enter a search Query: ")
     # print(inp)
     # create a query string based on the youtube apu and the search input
@@ -33,6 +48,13 @@ def main():
     # print out the results
     for v in results:
         print(v.title,v.channel,v.date,v.ID,v.thumbnail)
+
+
+def main():
+    """main"""
+    # ask the user for input
+    # current()
+    load_config()
 
 # if this file is the one that is run,
 if __name__ == "__main__":
